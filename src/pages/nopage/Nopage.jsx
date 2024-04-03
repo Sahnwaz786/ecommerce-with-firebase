144df1e2d7d732fada502a74808dceee0ad21baf
Sahnwaz786 <mdsahnwazalam48@gmail.com>
2024-04-04 00:32:56 +0530

    add and update product completed

---
+++ src/pages/nopage/Nopage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../../components/layout/Layout'
+
+const Nopage = () => {
+  return (
+    <div>
+        Nopage
+    </div>
+  )
+}
+
+export default Nopage
